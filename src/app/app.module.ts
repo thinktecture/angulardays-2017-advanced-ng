44acb8f4782bdd96090373e0795fabcf3ac6d0fd
Christian Liebel <christian.liebel@thinktecture.com>
2017-03-17 10:01:47 +0100

    ex1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
+
+// TODO: Add the camera service base class to the providers and use a factory to create an instance of the concrete camera service.
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent
   ],
   imports: [
     BrowserModule,
